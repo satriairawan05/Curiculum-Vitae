3d3bcbad40c393a57bb7b6bed169d95b03720753
satriairawan05 <satriai418@gmail.com>
2023-12-12 14:00:00 +0800

    Update axios.init.js

--- js/axios.init.js
+++ js/axios.init.js
@@ -1,16 +1,14 @@
-// Make a request for a user with a given ID
-axios.get('https://api.github.com/users/satriairawan05')
-.then(function (response) {
-// handle success
-    const img = document.getElementById('img');
-    img.src = response.data.avatar_url;
-    const repository = document.getElementById('repository');
-    repository.innerHTML = response.data.public_repos;
-})
-.catch(function (error) {
-// handle error
-console.log(error);
-})
-.finally(function () {
-// always executed
-});
+axios
+    .get("https://api.github.com/users/satriairawan05")
+    .then(function (response) {
+        const img = document.getElementById("img");
+        img.src = response.data.avatar_url;
+        const repository = document.getElementById("repository");
+        repository.innerHTML = response.data.public_repos;
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+    .finally(function () {
+        //
+    });
