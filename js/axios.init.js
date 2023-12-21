43c6bdbae28642615c887c94952c26630d813d47
satriairawan05 <satriai418@gmail.com>
2023-12-21 13:43:56 +0800

    Change Theme

--- js/axios.init.js
+++ js/axios.init.js
@@ -3,8 +3,6 @@ axios
     .then(function (response) {
         const img = document.getElementById("img");
         img.src = response.data.avatar_url;
-        const repository = document.getElementById("repository");
-        repository.innerHTML = response.data.public_repos;
     })
     .catch(function (error) {
         console.log(error);
